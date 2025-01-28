304db75e57bbb2494d32c923feac62731e78f094
gpego1 <gpego786@gmail.com>
2025-01-28 00:41:56 -0300

    Conexao com banco de dados clientes.mysql funcionando

--- cadastro-cliente/src/main/java/org/example/Main.java
+++ cadastro-cliente/src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/clientes";
+        String user = "root";
+        String password = "2605";
 
-        System.out.println("Hello, World!");
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Conexão bem-sucedida!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
